3c2fa1762bcb9bcd6d1c9a3f0f05f3a2bbc764f9
fursiontwo <yr0955@naver.com>
2023-08-25 20:01:54 +0900

    Commit 01_java_basic

---
+++ 01_java_basic/src/step1_06/loop/LoopEx28_풀이.java
@@ -0,0 +1,106 @@
+package step1_06.loop;
+
+
+// # 삼각형 그리기
+
+public class LoopEx28_풀이 {
+	
+	public static void main(String[] args) {
+		
+		/*
+		 * 예)
+		 * ###
+		 * ###
+		 * ###
+		 */
+		
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		/*
+		 * 문제 1)
+		 * #
+		 * ##
+		 * ###
+		 */
+		for (int i = 1; i < 4; i++) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		
+		/*
+		 * 문제 2)
+		 *   #
+		 *  ##
+		 * ###
+		 */
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < (3-i-1); j++) {
+				System.out.print(" ");
+			}
+			for (int j = 0; j < (i + 1); j++) {
+				System.out.println("#");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		/*
+		 * 문제 3)
+		 * ###
+		 * ##
+		 * #
+		 */
+		for (int i = 3; i >= 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		
+		/*
+		 * 문제 4)
+		 * ###
+		 *  ##
+		 *   #
+		 */
+
+		
+		/*
+		 * 문제 5)
+		 * @##
+		 * @@#
+		 * @@@
+		 */
+
+		
+		/*
+		 * 문제 6)
+		 *   #
+		 *  ###
+		 * #####
+		 */
+
+		/*
+		 * 문제 7)
+		 *  #####
+		 *   ###
+		 *    #
+		 */
+
+		
+		
+	}
+}
